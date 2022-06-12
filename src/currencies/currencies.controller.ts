0f88c839819e222c5957dbf8140e92987dde0d5b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-06-12 19:20:56 -0300

    create method createCurrency in currencies controller

--- src/currencies/currencies.controller.ts
+++ src/currencies/currencies.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Currencies } from './currencies.entity';
 import { CurrenciesService } from './currencies.service';
+import { CreateCurrencyDTO } from './dto/create-currency.dto';
 
 @Controller('currencies')
 export class CurrenciesController {
@@ -9,5 +19,13 @@ export class CurrenciesController {
   @Get('/:currency')
   async getCurrency(@Param('currency') currency: string): Promise<Currencies> {
     return await this.currenciesService.getCurrency(currency);
+  }
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCurrency(
+    @Body() createCurrencyDTO: CreateCurrencyDTO,
+  ): Promise<Currencies> {
+    return await this.currenciesService.createCurrency(createCurrencyDTO);
   }
 }
